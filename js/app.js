a8cc5eb2505ec6e327ff86a9cc1ef35336a9781f
ivandobrica <dobrica.ivan@hotmail.com>
2021-09-08 13:36:45 +0200

    Banner

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+// 100vh on mobile when adress bar is present
+function appHeight() {
+  const doc = document.documentElement;
+  doc.style.setProperty('--vh', (window.innerHeight*.01) + 'px');
+}
+window.addEventListener('resize', appHeight);
+appHeight();
+
+
 //horizontal scrollbar
 window.onscroll = function() {myFunction()};
 function myFunction() {
