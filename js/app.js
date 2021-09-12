2d47425109d8d3234e0fa099f2cd62def51c70a2
ivandobrica <dobrica.ivan@hotmail.com>
2021-09-12 19:32:25 +0200

    Cursor circle, code refactored

--- js/app.js
+++ js/app.js
@@ -10,30 +10,30 @@ appHeight();
 //horizontal scrollbar
 window.onscroll = function() {myFunction()};
 function myFunction() {
-  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  let scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
+      height = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+      scrolled = (winScroll / height) * 100;
+  document.querySelector(".progress-bar").style.width = scrolled + "%";
 }
 
 
 //page loader
-// let body = document.querySelector("body");
-// window.addEventListener("load", () => {
-//   const preload = document.querySelector(".preload-wrapper");
-//   preload.classList.add("preload-finish");
-//   setTimeout(function() {
-//     body.classList.remove("no-scroll");
-//   }, 5000);
-// });
+let body = document.querySelector("body");
+window.addEventListener("load", () => {
+  const preload = document.querySelector(".preload-wrapper");
+  preload.classList.add("preload-finish");
+  setTimeout(function() {
+    body.classList.remove("no-scroll");
+  }, 5000);
+});
 
 
 //hamburger action
-let hamburger = document.querySelector(".hamburger");
-let link = document.querySelector(".overlay__item");
-let overlay = document.querySelector(".overlay");
-let navigationList = document.querySelector(".navigation__list");
-let overlayAction = [hamburger, link];
+let hamburger = document.querySelector(".hamburger"),
+    link = document.querySelector(".overlay__item"),
+    overlay = document.querySelector(".overlay"),
+    navigationList = document.querySelector(".navigation__list"),
+    overlayAction = [hamburger, link];
 for(let i = 0; i < overlayAction.length; i++) {
   overlayAction[i].addEventListener("click", function () {
     overlay.classList.toggle("is-open");
@@ -45,21 +45,20 @@ for(let i = 0; i < overlayAction.length; i++) {
 
 
 //projects horizontal scroll
-let vwWidth = window.innerWidth;
-let controller = new ScrollMagic.Controller();
-
-let horizontalSlide = new TimelineMax()
-  .to(".horizontal__wrapper", 0.5, {x: 0, ease: Power1.easeOut}, )
-  .to(".horizontal__wrapper", 1, {x: -vwWidth, ease: Power1.easeOut}, )	
-  .to(".horizontal__wrapper", 0.5, {x: -vwWidth, ease: Power1.easeOut}, )
-  .to(".horizontal__wrapper", 1, {x: -2*vwWidth, ease: Power1.easeOut}, )
-  .to(".horizontal__wrapper", 0.5, {x: -2*vwWidth, ease: Power1.easeOut}, )
-  .to(".horizontal__wrapper", 1, {x: -3*vwWidth, ease: Power1.easeOut}, )
-  .to(".horizontal__wrapper", 0.5, {x: -3*vwWidth, ease: Power1.easeOut}, )
-  .to(".horizontal__wrapper", 1, {x: -4*vwWidth, ease: Power1.easeOut}, )
-  .to(".horizontal__wrapper", 0.5, {x: -4*vwWidth, ease: Power1.easeOut}, )
-  .to(".horizontal__wrapper", 1, {x: -5*vwWidth, ease: Power1.easeOut}, )
-  .to(".horizontal__wrapper", 0.5, {x: -5*vwWidth, ease: Power1.easeOut}, )
+let vwWidth = window.innerWidth,
+    controller = new ScrollMagic.Controller(),
+    horizontalSlide = new TimelineMax()
+    .to(".horizontal__wrapper", 0.5, {x: 0, ease: Power1.easeOut}, )
+    .to(".horizontal__wrapper", 1, {x: -vwWidth, ease: Power1.easeOut}, )	
+    .to(".horizontal__wrapper", 0.5, {x: -vwWidth, ease: Power1.easeOut}, )
+    .to(".horizontal__wrapper", 1, {x: -2*vwWidth, ease: Power1.easeOut}, )
+    .to(".horizontal__wrapper", 0.5, {x: -2*vwWidth, ease: Power1.easeOut}, )
+    .to(".horizontal__wrapper", 1, {x: -3*vwWidth, ease: Power1.easeOut}, )
+    .to(".horizontal__wrapper", 0.5, {x: -3*vwWidth, ease: Power1.easeOut}, )
+    .to(".horizontal__wrapper", 1, {x: -4*vwWidth, ease: Power1.easeOut}, )
+    .to(".horizontal__wrapper", 0.5, {x: -4*vwWidth, ease: Power1.easeOut}, )
+    .to(".horizontal__wrapper", 1, {x: -5*vwWidth, ease: Power1.easeOut}, )
+    .to(".horizontal__wrapper", 0.5, {x: -5*vwWidth, ease: Power1.easeOut}, )
 
 new ScrollMagic.Scene({
   triggerElement: ".horizontal__wrapper",
@@ -73,8 +72,8 @@ new ScrollMagic.Scene({
 
 
 //project view more button
-let projectImage = document.querySelectorAll(".project__image");
-let viewMore = document.querySelectorAll(".project__view-more");
+let projectImage = document.querySelectorAll(".project__image"),
+    viewMore = document.querySelectorAll(".project__view-more");
 for(let i = 0; i < viewMore.length; i++) {
   viewMore[i].addEventListener("mouseover", function() {
     projectImage[i].classList.remove("filter");
@@ -88,9 +87,9 @@ for(let i = 0; i < viewMore.length; i++) {
 
 
 //remove about me image, show CV
-let showCV = document.querySelector(".about__next");
-let imgContainer = document.querySelector(".about__info");
-let showImg = document.querySelector(".about__back");
+let showCV = document.querySelector(".about__next"), 
+    imgContainer = document.querySelector(".about__info"), 
+    showImg = document.querySelector(".about__back");
 showCV.addEventListener("click", function() {
   imgContainer.classList.add("is-open");
   showImg.classList.remove("hidden");
@@ -101,3 +100,27 @@ showImg.addEventListener("click", function() {
   showImg.classList.add("hidden");
   showCV.classList.remove("hidden");
 })
+
+
+//mouse cursor circle
+let mousePosX = 0,
+    mousePosY = 0,
+    mouseCircle = document.querySelector(".mouse-circle");
+
+document.onmousemove = (e) => {
+    mousePosX = e.pageX;
+    mousePosY = e.pageY;
+};
+
+let delay = 6,
+    revisedMousePosX = 0,
+    revisedMousePosY = 0;
+
+function delayMouseFollow() {
+  requestAnimationFrame(delayMouseFollow);
+  revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
+  revisedMousePosY += (mousePosY - revisedMousePosY) / delay;
+  mouseCircle.style.top = revisedMousePosY + "px";
+  mouseCircle.style.left = revisedMousePosX + "px";
+}
+delayMouseFollow();
